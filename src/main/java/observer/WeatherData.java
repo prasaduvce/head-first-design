6ff2fdc49934de3b36b3c3e7ee5dcde57abd97c3
prasaduvce <prasaduvce@gmail.com>
2023-07-17 20:30:01 +0530

    Observer pattern initial copy

---
+++ src/main/java/observer/WeatherData.java
@@ -0,0 +1,42 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    private List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer: observers
+             ) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
